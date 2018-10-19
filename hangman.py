6a30675d5ef88652e3f3982d79ff0c5804518e26
Lizzie Courtney <escourtney@gmail.com>
2018-10-19 16:05:33 +0100

    Initial Commit

---
+++ hangman.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+##Hangman
+## Computer chooses a word
+## Player guesses the letters of the word
+
+word = "hangman"
+#print(word)
+wrong_count = 0
+right_count = 0
+
+#use set rather than list because set only contains unique values
+correct_letters=set(word)
+
+#for letter in word:
+ #   if letter not in correct_letters:
+  #      correct_letters.append(letter)
+
+print(correct_letters)
+
+correct_guesses=[]
+
+
+while (wrong_count < 10):
+        
+    guess = input("Guess a letter: ")
+    print (guess)
+    
+    if guess in word:
+        print("Success!")
+        if guess not in correct_guesses:
+            correct_guesses.append(guess)
+            if correct_guesses == correct_letters:
+                print("You win!")
+                break
+        else:
+            print("Already guessed that letter!")
+        print(correct_guesses)
+        
+        #right_count +=1
+        #print ("Correct guesses: " + str(right_count))
+        #break
+    else:
+        print("Bad luck")
+        wrong_count +=1
+        print ("Wrong guesses: " + str(wrong_count))
